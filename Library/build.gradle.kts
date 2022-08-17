eda845c2a2c827ea519bc8011422dcbd89468df4
JinnosukeKato <96569057+jinnosukeKato@users.noreply.github.com>
2022-08-17 15:32:58 +0900

    [fix] artifactらへん追加

--- Library/build.gradle.kts
+++ Library/build.gradle.kts
@@ -9,10 +9,26 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
-    withSourcesJar()
-    withJavadocJar()
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:$mcVersion-R0.1-SNAPSHOT")
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            groupId = rootProject.group as String?
+            artifactId = rootProject.name
+            version = rootProject.version as String?
+        }
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 allprojects {
-    version = "1.0.0"
+    version = "1.0.1"
     group = "io.github.jinnosukeKato"
 }
 
